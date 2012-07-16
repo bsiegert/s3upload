b16c9c56a6cddc8b745cd534fffe57361638c5d9
Benny Siegert <bsiegert@gmail.com>
2012-07-16 10:36:20 +0000

    Rough first prototype.
    It should already be able to upload something.

--- s3upload.go
+++ s3upload.go
@@ -3,15 +3,22 @@ package main
 import (
 	"bytes"
 	"flag"
-	"fmt"
 	"io/ioutil"
 	"log"
+	"mime"
 	"os"
 	"os/user"
+	"path"
+	"path/filepath"
 	"strings"
+	"sync"
 
 	"launchpad.net/goamz/aws"
 	"launchpad.net/goamz/s3"
+)
+
+var (
+	nConn = flag.Int("n", 3, "maximum number of connections")
 )
 
 func ParseURL(url string) (bucket string, path string) {
@@ -60,7 +67,7 @@ func main() {
 	}
 
 	args := flag.Args()
-	if len(args) == 0 {
+	if len(args) < 2 {
 		flag.Usage()
 		return
 	}
@@ -72,11 +79,11 @@ func main() {
 	if bucketname == "" {
 		log.Fatalf("invalid target URL: %s", target)
 	}
-	log.Print(bucketname, bpath)
 
 	conn := s3.New(auth, aws.EUWest)
 	bucket := conn.Bucket(bucketname)
 
+	/*
 	list, err := bucket.List(bpath, "", "", 100)
 	if err != nil {
 		log.Fatal(err)
@@ -84,5 +91,68 @@ func main() {
 	for _, item := range list.Contents {
 		fmt.Printf("%s\t%d\n", item.Key, item.Size)
 	}
-}
+	*/
 
+	// Start nConn upload routines
+	var wg sync.WaitGroup
+	wg.Add(*nConn)
+	c := make(chan string, *nConn)
+	for i := 0; i < *nConn; i++ {
+		go func() {
+			for filename := range c {
+				fi, err := os.Stat(filename)
+				if err != nil {
+					log.Print(err)
+					continue
+				}
+				f, err := os.Open(filename)
+				if err != nil {
+					log.Print(err)
+					continue
+				}
+
+				mimetype := mime.TypeByExtension(filepath.Ext(filename))
+				if mimetype == "" {
+					mimetype = "application/octet-stream"
+				}
+				tpath := path.Clean(path.Join(bpath, fi.Name()))
+				log.Print(filename)
+				err = bucket.PutReader(tpath, f, fi.Size(), mimetype, s3.PublicRead)
+				if err == nil {
+					f.Close()
+					continue
+				}
+				// Infinite retries ...
+				for i := 1; err != nil; i++ {
+					log.Printf("Error uploading %s: %s", tpath, err.Error())
+					f.Seek(0, 0)
+					log.Printf("%s (retry %d)", tpath, i)
+					err = bucket.PutReader(tpath, f, fi.Size(), mimetype, s3.PublicRead)
+				}
+				f.Close()
+			}
+			wg.Done()
+		}()
+	}
+
+	// Feed file names.
+	for _, a := range args {
+		fi, err := os.Stat(a)
+		if err != nil {
+			log.Printf("skipping %s: %s", a, err.Error())
+			continue
+		}
+		if fi.IsDir() {
+			filepath.Walk(a, func(path string, fi os.FileInfo, err error) error {
+				if err != nil && !fi.IsDir() {
+					c <- path
+				}
+				return nil
+			})
+		} else {
+			c <- a
+		}
+	}
+	close(c)
+	wg.Wait()
+}
